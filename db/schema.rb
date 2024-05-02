4038105ea521728adfda753d1ebe7c82ce8fdef8
Lucy Hollingsworth <lucyholl@me.com>
2024-05-02 16:55:04 +0100

    cards homepage grid layout

--- db/schema.rb
+++ db/schema.rb
@@ -64,15 +64,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_131709) do
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
