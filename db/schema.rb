fcb76dbabdfc4e42d50c5afaf5e7d4f0d55c48be
Maria del Mar Sevilla Escobar <sevillamariadelmar@gmail.com>
2024-05-02 14:12:12 +0200

    navbar new style

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_05_01_131709) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,15 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_131709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_characters_on_user_id"
-  end
-
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
