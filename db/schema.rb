7bd3a4626a1b8b8aa10241c86396465aec5e99b8
Silja Stoicescu <siljastoicescu@gmail.com>
2024-05-03 10:02:02 +0200

    made banners vh size and searchbar larger

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_05_02_091140) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +64,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_091140) do
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
